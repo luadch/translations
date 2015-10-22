64ff15bd18e5651be26bdb42bebf930188d10500
Sopor- <sopor@hotmail.com>
2015-10-22 22:30:26 +0200

    added hub_runtime.lang.sv

---
+++ Swedish/src/scripts/lang/hub_runtime.lang.sv
@@ -0,0 +1,22 @@
+﻿return {
+
+    help_title = "hub_runtime.lua",
+    help_usage = "[+!#]runtime show|reset",
+    help_desc = "Visa/nollställ hubbens drifttid",
+
+    msg_runtime = "Hubbens drifttid: %s",
+    msg_reset_1 = "Hubbens drifttid är nollställd.",
+    msg_reset_2 = "Hubbens drifttid är nollställd av: %s",
+    msg_denied = "Du har inte behörighet att använda detta kommando.",
+    msg_usage = "Användning: [+!#]runtime show|reset",
+
+    msg_years = " år, ",
+    msg_days = " dagar, ",
+    msg_hours = " timmar, ",
+    msg_minutes = " minuter, ",
+    msg_seconds = " sekunder",
+
+    ucmd_menu_show = { "Hubb", "Kärna", "Hubbens drifttid", "visa" },
+    ucmd_menu_reset = { "Hubb", "Kärna", "Hubbens drifttid", "nollställ", "OK" },
+
+}

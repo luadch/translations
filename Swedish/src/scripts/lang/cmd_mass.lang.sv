53162c0a77272fd8a6bf042823e8b8012d3691be
Sopor- <sopor@hotmail.com>
2015-12-19 21:10:35 +0100

    - updated cmd_mass.lang.sv - added usr_hide_share.lang.sv

--- Swedish/src/scripts/lang/cmd_mass.lang.sv
+++ Swedish/src/scripts/lang/cmd_mass.lang.sv
@@ -3,21 +3,22 @@
     help_title = "cmd_mass.lua - Users",
     help_usage = "[+!#]mass <MEDDELANDE>",
     help_desc = "Skickar ett PM med <MEDDELANDE> till alla användare",
-    
-    help_title_op = "cmd_mass.lua - Ops",
-    help_usage_op = "[+!#]masslvl <ÅTKOMSTNIVÅ> <MEDDELANDE>",
-    help_desc_op = "Skickar ett PM med <MEDDELANDE> till alla användare med särskild åtkomstnivå",
 
-    ucmd_menu = { "Användare", "Meddelande", "Gruppmeddelande", "till ALLA" },
+    help_title_op = "cmd_mass.lua - Ops",
+    help_usage_op = "[+!#]masslvl <ÅTKOMSTNIVÅ> <MEDDELANDE> / [+!#]masshub <MEDDELANDE>",
+    help_desc_op = "Skickar ett PM med <MEDDELANDE> till alla användare med särskild åtkomstnivå / skickar ett PM med <MEDDELANDE> utan avsändare",
+
+    ucmd_menu = { "Användare", "Meddelande", "Gruppmeddelande", "till alla" },
+    ucmd_menu_hub = { "Användare", "Meddelande", "Gruppmeddelande", "till alla (utan avsändare)" },
     ucmd_menu_1 = "Användare",
     ucmd_menu_2 = "Meddelande",
     ucmd_menu_3 = "Gruppmeddelande",
-    ucmd_menu_4 = "till ÅTKOMSTNIVÅ",
+    ucmd_menu_4 = "till åtkomstnivå",
     ucmd_what =  "Meddelande:",
 
     msg_denied = "Du har inte behörighet att använda detta kommando.",
     msg_usage = "Användning: [+!#]mass <MEDDELANDE>",
-    msg_usage_op = "Anvndning: [+!#]masslvl <ÅTKOMSTNIVÅ> <MEDDELANDE>",
+    msg_usage_op = "Användning: [+!#]masslvl <ÅTKOMSTNIVÅ> <MEDDELANDE> / [+!#]masshub <MEDDELANDE>",
     msg_lvl_exists = "Åtkomstnivån %s finns inte.",
     msg_ok = "Gruppmeddelande har skickats ut till alla användare med åtkomstnivå: ",
     msg_out = [[
@@ -31,13 +32,25 @@ Meddelande:  %s
 
 ====================================================================================================== GRUPPMEDDELANDE  ===
   ]],
-  
-    msg_out_op = [[
+
+    msg_out_lvl = [[
 
 
 === GRUPPMEDDELANDE  ======================================================================================================
 
 Avsändare:  %s   |   Datum:  %s   |   Tid:  %s  |  Skickat till alla användare med åtkomstnivå:  %s
+
+Meddelande:  %s
+
+====================================================================================================== GRUPPMEDDELANDE  ===
+  ]],
+
+    msg_out_hub = [[
+
+
+=== GRUPPMEDDELANDE  ======================================================================================================
+
+Datum:  %s   |   Tid:  %s
 
 Meddelande:  %s
 
---
+++ Swedish/src/scripts/lang/usr_hide_share.lang.sv
@@ -0,0 +1,20 @@
+﻿return {
+
+    help_title = "usr_hide_share.lua",
+    help_usage = "[+!#]hideshare <ANVÄNDARNAMN>",
+    help_desc = "Göm/visa utdelningen för en användare",
+
+    msg_denied = "Du har inte behörighet att använda detta kommando.",
+    msg_isbot = "Användaren är en bot.",
+    msg_notonline = "Användaren är frånkopplad.",
+    msg_usage = "Användning: [+!#]hideshare <ANVÄNDARNAMN>",
+
+    msg_default = "Åtkomstnivån bestämmer att denna användares utdelning är gömd.",
+    msg_hide_user = "Utdelning är gömd för: %s",
+    msg_hide_target = "Din utdelning gömdes av: %s",
+    msg_unhide_user = "Utdelningen visas igen för: %s  |  Användaren blev frånkopplad",
+    msg_unhide_target = "Din utdelning har återställts av: %s  |  Därför kommer du att bli frånkopplad nu",
+
+    ucmd_menu_ct2_1 = { "Ändra", "Utdelning", "göm//visa igen" },
+
+}
